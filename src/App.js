0b80e8c32affb3d3cbecb75cacbd0211989b7189
nhlchrs <nhlchaurasia1234@gmail.com>
2020-10-21 00:07:40 +0530

    LocalStorage

--- src/App.js
+++ src/App.js
@@ -3,35 +3,40 @@ import React,{useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const intialState= JSON.parse(localStorage.getItem("tasks"))||[];
   const [todo, setTodo] = useState('')
-  const[ tasks, setTask]= useState([{id:1,task:'HElo'}])
+  const[ tasks, setTask]= useState(intialState)
   const [editItem, setEditItem]= useState(null)
 
+const editTask =(id,task)=>{
+  const newTask = tasks.map(item=>item.id===id ? {id,task} : item)
+  setTask(newTask)
+}
 
-
-  const editTask=(id,task)=>{
-    const newTask = tasks.map(item=>item.id===id ? {id,task} : item)
-    setTask(newTask)
-  }
-
-  
+useEffect(()=>{
+  localStorage.setItem("tasks",JSON.stringify(tasks))
+},[tasks])
 const addTask=(todo)=>{
   if(editItem===null){
-  if(todo!==''){
 setTask([...tasks,{id:Math.random(), task:todo}])
-setTodo("")
+setTodo("")  
   }
   else{
-    alert("withOut name")
-  }}
-  else{
-    editTask(editItem.id, todo)
+    editTask(editItem.id, todo )
+    console.log(editItem.id)
     setTodo("")
     setEditItem(null)
   }
-
-  
 }
+useEffect(()=>{
+  if(editItem!==null){
+    setTodo(editItem.task)
+  }
+  else{
+    setTodo("")
+  }
+},[editItem]
+)
 const clearTask=()=>{
   setTask([])
 }
@@ -39,31 +44,19 @@ const deleteTask=(id)=>{
   alert(id)
   setTask([...tasks.filter(item=>item.id!==id)])
 }
-console.log(tasks)
 
 const findItem=(id)=>{
-  alert(id)
   setEditItem(tasks.find(item=>item.id===id))
 }
 
-
-useEffect(() => {
-  if(editItem!==null){
-    console.log(editItem.task)
-    setTodo(editItem.task)
-  }
-  else{
-    setTodo("")
-  }
-}, [editItem])
   return (
     <div className="App">
      <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
      <button onClick={()=>addTask(todo)}> {editItem ===null ? 'ADD' : 'UPDATE'}</button>
      <button onClick={()=>clearTask()}>CLEAR</button>
 
-    {tasks.map(item=>(
-      <div>
+    {tasks.map((item,index)=>(
+      <div key={index}>
       <h1>{item.task}</h1>
       <button onClick={()=>deleteTask(item.id)}>Del</button>
       <button onClick={()=>findItem(item.id)}>find</button>
